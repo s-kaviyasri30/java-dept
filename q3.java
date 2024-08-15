fbd6b5e140f8e183e874987e4351959a484b0801
s-kaviyasri30 <160023528+s-kaviyasri30@users.noreply.github.com>
2024-08-15 23:33:31 +0530

    Create q3.java

---
+++ q3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class q3
+{
+    Scanner hello = new Scanner(System.in);
+    String name = "kaviyasri";
+    int score = 56;
+    String name = "cse";
+    System.out.println("My name is ")
+    System.out.println("My score is ")
+    System.out.print("My department is ")
+}
+            
